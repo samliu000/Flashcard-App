e9421b7ba6f9b30d13a6e6bb3d7375b092a375e9
samliu000 <samuel.liu1004@gmail.com>
2020-02-21 21:57:58 -0800

    Added eye icon

--- app/src/main/java/com/example/samuelliu/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/samuelliu/flashcardapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.samuelliu.flashcardapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,20 +12,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.Question).setElevation(20);
 
-        findViewById(R.id.Question).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.ps).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.Question).setVisibility(View.INVISIBLE);
-                findViewById(R.id.Answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.ps).setBackgroundColor(Color.parseColor("#fc6565"));
+                findViewById(R.id.kc).setBackgroundColor(Color.parseColor("#40d12a"));
+                findViewById(R.id.sf).setBackgroundColor(Color.parseColor("#d2cced"));
             }
         });
 
-        findViewById(R.id.Answer).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.kc).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.Answer).setVisibility(View.INVISIBLE);
-                findViewById(R.id.Question).setVisibility(View.VISIBLE);
+                findViewById(R.id.kc).setBackgroundColor(Color.parseColor("#40d12a"));
+                findViewById(R.id.ps).setBackgroundColor(Color.parseColor("#d2cced"));
+                findViewById(R.id.sf).setBackgroundColor(Color.parseColor("#d2cced"));
+            }
+        });
+
+        findViewById(R.id.sf).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.sf).setBackgroundColor(Color.parseColor("#fc6565"));
+                findViewById(R.id.kc).setBackgroundColor(Color.parseColor("#40d12a"));
+                findViewById(R.id.ps).setBackgroundColor(Color.parseColor("#d2cced"));
             }
         });
 
